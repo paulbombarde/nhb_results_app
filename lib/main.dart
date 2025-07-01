aa4d5128cba09f62d17c291abcfe466a258fac4f
Paul Bombarde <pbombarde@logitech.com>
2025-07-01 22:36:00 +0200

    Remove unused old code

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'screens/loading_screen.dart';
 
